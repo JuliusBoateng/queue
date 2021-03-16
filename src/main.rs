46a664bb4b9bf4985c7f9ccb1b313f2fcf174b82
Stephen Hannon <stephenhannon4@gmail.com>
2021-03-15 21:18:32 -0400

    Actix basics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[get("/{name}")]
+async fn index(web::Path(name): web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port:u32 = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(index)
+    })
+    .bind(format!("0.0.0.0:{}", port))?
+    .run()
+    .await
 }
 
