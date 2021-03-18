5cc65e8b5d9a81e0105b108d53f3a6d5470fc2ee
Stephen Hannon <stephenhannon4@gmail.com>
2021-03-18 15:40:56 -0400

    Add serde derivations

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,14 @@
 use actix_web::{get, web, HttpResponse, Responder};
+use chrono::Utc;
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
+    HttpResponse::Ok().json(queue::Student {
+        id: "abc1234".to_string(),
+        name: "John Smith".to_string(),
+        time: Utc::now(),
+        desc: "Help with queues".to_string(),
+    })
 }
 
 #[get("/{name}")]
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
-use chrono::{NaiveDateTime};
+use chrono::{DateTime, Utc};
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Queue {
     pub id: String,
     pub course: String,
     pub name: String,
-    pub start: NaiveDateTime,
-    pub end: NaiveDateTime,
+    pub start: DateTime<Utc>,
+    pub end: DateTime<Utc>,
     pub location: String,
     pub students: Vec<Student>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Student {
     pub id: String,
     pub name: String,
+    pub time: DateTime<Utc>,
     pub desc: String,
 }
