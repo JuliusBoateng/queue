1eb8de5e5da1a44a72a95f155ad55ca277b0a866
Emory Smith <emorysmithis@gmail.com>
2021-03-18 21:01:51 -0400

    this works

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
 use actix_web::{App, HttpServer};
 use std::env;
-use mongodb::{Client, options::ClientOptions};
 
 mod api; 
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    // Parse a connection string into an options struct.
-    let db_connect = env::var("DBCONNECT").expect("Missing DBCONNECT environment variable");
-    let mut client_options = ClientOptions::parse(&db_connect).await.expect("Can't Connect to Mongo");
-    
-    // Manually set an option.
-    client_options.app_name = Some("My App".to_string());
-
-    // Get a handle to the deployment.
-    let client = Client::with_options(client_options).expect("Can't get a handle to deployment");
-
-    // List the names of the databases in that deployment.
-    for db_name in client.list_database_names(None, None).await.expect("Can't list database names") {
-        println!("It worked: {}", db_name);
-    }
-
+async fn main() -> std::io::Result<()> { 
     let port:u32 = env::var("PORT")
         .unwrap_or_else(|_| "3000".to_string())
         .parse()
